629993fd23cdd8bcbe4b5fff95b62cf974de30ec
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-12 11:52:17 +0700

    служба входа-выхода

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -2,17 +2,47 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the AuthProvider provider.
+import firebase from 'firebase';
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello AuthProvider Provider');
-  }
+	constructor(public http: Http) {
+		console.info('AuthProvider.constructor()');
+	}
+
+
+	signupUser(email: string, password: string): firebase.Promise<any> {
+		return firebase.auth().createUserWithEmailAndPassword(email, password)
+			.then( newUser => {
+				firebase.database().ref('userProfile').child(newUser.uid)
+					.set({ email: email });
+			});
+	}
+
+
+	loginUser(email: string, password: string): firebase.Promise<any> {
+		// console.info('AuthProvider.loginUser()');
+		return firebase.auth().signInWithEmailAndPassword(email, password)
+			.then( () => {
+				console.info('ОБЕЩАЛ ЗАЙТИ');
+			});
+	}
+
+
+	resetPassword(email: string): firebase.Promise<void> {
+		return firebase.auth().sendPasswordResetEmail(email)
+			.then( () => {
+				console.info('ОБЕЩАЛ ВСПОМНИТЬ');
+			});
+	}
+
+
+	logoutUser(): firebase.Promise<void> {
+		return firebase.auth().signOut()
+			.then( () => {
+				console.info('ОБЕЩАЛ УЙТИ');
+			});
+	}
 
 }
