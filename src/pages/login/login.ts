162480d92da748b41d05f8e2b88b189916f01428
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-15 13:42:51 +0700

    Revert "*"
    This reverts commit 007928308625874727bb5d89776dfb3d36729207.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,78 +1,24 @@
 import { Component } from '@angular/core';
-import {
-  IonicPage, 
-  Loading,
-  LoadingController, 
-  NavController,
-  AlertController,
-} from 'ionic-angular';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { EmailValidator } from '../../validators/email';
-import { AuthProvider } from '../../providers/auth/auth';
-import { HomePage } from '../home/home';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-@IonicPage({
-  name: 'login'
-})
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage({ name: 'login' })
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  public loginForm: FormGroup;
-  loading: Loading;
 
-  constructor(
-  	public navCtrl: NavController, 
-  	public loadingCtrl: LoadingController, 
-    public alertCtrl: AlertController, 
-    public formBuilder: FormBuilder,
-    public authProvider: AuthProvider, 
-    ) {
-      this.loginForm = formBuilder.group({
-        email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
-        password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
-      });
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  loginUser(): void {
-    if (!this.loginForm.valid){
-      console.log(this.loginForm.value);
-    } else {
-      this.authProvider.loginUser(this.loginForm.value.email, this.loginForm.value.password)
-      .then( authData => {
-        this.loading.dismiss().then( () => {
-          this.navCtrl.setRoot(HomePage);
-        });
-      }, error => {
-        this.loading.dismiss().then( () => {
-          let alert = this.alertCtrl.create({
-            message: error.message,
-            buttons: [
-              {
-                text: "Угу",
-                role: 'cancel'
-              }
-            ]
-          });
-          alert.present();
-        });
-      });
-      
-      // попробовать убрать
-      this.loading = this.loadingCtrl.create();
-      this.loading.present();
-    }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
   }
-
-  goToSignup(): void {
-    this.navCtrl.push('signup');
-  }
-
-  goToResetPassword(): void {
-    this.navCtrl.push('reset-password');
-  }
-
-  
 
 }
